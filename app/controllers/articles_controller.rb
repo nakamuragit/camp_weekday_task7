0946c6220fb9226d7ea8393dca8de39cb510f3e1
Nakamura <gireru@hotmail.co.jp>
2019-03-13 18:22:34 +0900

    インデント修正,リファクタリング

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -18,23 +18,25 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
     redirect_to @article
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
   private
 
   def article_params 
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_post
+    @article = Article.find(params[:id])
   end
 end
